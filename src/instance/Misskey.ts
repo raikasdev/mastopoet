b4812d8771f9cf6e05bf5f7600897de1478a9c09
Ikramullah <ikramullahzx@gmail.com>
2023-08-01 22:18:53 +0800

    feat(instance): add misskey instance (not implemented tho)

--- src/instance/Misskey.ts
+++ src/instance/Misskey.ts
@@ -1,9 +1,7 @@
-import { Post } from "../components/PostItem";
 import BaseInstance from "./BaseInstance";
 
-export default class MisskeyInstance extends BaseInstance {
+export default class MastodonInstance extends BaseInstance {
   public async execute(): Promise<any> {
-    console.log("Hello world! [Misskey]");
-    return "";
+    throw new Error("Misskey support not implemented")
   }
 }
