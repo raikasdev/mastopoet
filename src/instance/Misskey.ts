ee3e2c2e753ce651201986282054fabee67c6759
Ikramullah <ikramullahzx@gmail.com>
2023-08-22 00:55:36 +0800

    feat(instance/misskey): roughly fitting Misskey output

--- src/instance/Misskey.ts
+++ src/instance/Misskey.ts
@@ -1,5 +1,8 @@
-import { Post } from "../components/PostItem";
+import { AxiosError } from "axios";
+import { Attachment, Post } from "../components/PostItem";
+import { truncateString } from "../utils/util";
 import BaseInstance from "./BaseInstance";
+import { axiosInstance } from "../utils/axios";
 
 interface MisskeyReaction {
   [emojiName: string]: string | number;
@@ -90,6 +93,57 @@ export default class MisskeyInstance extends BaseInstance {
   public async execute(): Promise<Post> {
     // TODO: instance/api/notes/show [POST]
     // TODO: instance/api/users/show [POST] [If there's mentions over there]
-    throw new Error("Misskey/Firefish is not supported yet.")
+    if (this.url.protocol !== 'https:')
+      throw new Error("Protocol must be HTTPS");
+
+    try {
+      const uri = new URL(`https://${this.url.host}/api/notes/show`);
+      const note = await axiosInstance.post(uri.toString(), { noteId: this.postId });
+      const dataNote: MisskeyNotesResponse = note.data;
+
+      console.log(dataNote);
+
+      const username = `@${dataNote.user.username}@${this.url.host}`
+
+      const attachments: Attachment[] = dataNote.files.map(val => {
+        return {
+          type: 'image',
+          url: val.url,
+          aspectRatio: 1,
+          description: val.comment
+        }
+      });
+      console.log(attachments);
+
+      return {
+        username,
+        attachments,
+        displayName: dataNote.user.name,
+        plainUsername: dataNote.user.username,
+        avatarUrl: dataNote.user.avatarUrl,
+        content: dataNote.text,
+        boosts: dataNote.renoteCount,
+        comments: dataNote.repliesCount,
+        favourites: this.parseReactionToFavourites(dataNote.reactions),
+        date: new Date(dataNote.createdAt)
+      }
+    } catch (e) {
+      if (e instanceof AxiosError) {
+        if (!e.response) throw new Error("Failed to reach API");
+        if (e.response.status === 404)
+          throw new Error("Post not found. Is it private?");
+      }
+      throw new Error("Unknown error trying to reach Misskey instance");
+    }
+  }
+
+  // TODO: Can you change this to reaction list like Misskey things?
+  private parseReactionToFavourites(reaction: MisskeyReaction): number {
+    let count = 0;
+    Object.keys(reaction).forEach(react => {
+      if (typeof reaction[react] !== 'number') return;
+      count += reaction[react] as number;
+    });
+    return count;
   }
 }
