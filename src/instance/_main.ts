c200ab70fe0b65947202ddf436c651b32c4ed3a9
raikasdev <roni.aikas@gmail.com>
2023-10-11 15:30:48 +0300

    Add Akkoma support

--- src/instance/_main.ts
+++ src/instance/_main.ts
@@ -1,36 +1,46 @@
-import { Post } from '../components/PostItem';
-import BaseInstance from './BaseInstance';
+import { Post } from "../components/PostItem";
+import BaseInstance from "./BaseInstance";
 import MisskeyInstance from "./Misskey";
-import MastodonInstance from './Mastodon';
+import MastodonInstance from "./Mastodon";
 
 // Since all script on ReactJS will be imported to frontend,
 // we cannot dynamically import them using File System NodeJS.
-type InstanceListType = [typeof BaseInstance, RegExp[]][]
+type InstanceListType = [typeof BaseInstance, RegExp[]][];
 const Instances: InstanceListType = [
-  [MastodonInstance, [
-    /^\/@\w+(?:@[\w-.]+)?\/(\d+)$/, // instace.social/@user/postid
-    /^\/users\/\w+(?:@[\w-.]+)?\/statuses\/(\d+)$/, // instance.social/users/user/statuses/postid
-  ]],
-  [MisskeyInstance, [
-    /^\/notes\/(\w+)$/, // instace.social/notes/postid
-  ]]
+  [
+    MastodonInstance,
+    [
+      /^\/@\w+(?:@[\w-.]+)?\/(\d+)$/, // instace.social/@user/postid
+      /^\/users\/\w+(?:@[\w-.]+)?\/statuses\/(\d+)$/, // instance.social/users/user/statuses/postid
+      /^\/@\w+(?:@[\w-.]+)?\/posts\/(\d+)$/, // Akkoma, instance.social/@user/posts/postid
+    ],
+  ],
+  [
+    MisskeyInstance,
+    [
+      /^\/notes\/(\w+)$/, // instace.social/notes/postid
+    ],
+  ],
 ];
-export default async function(inputURL: string): Promise<Post> {
+export default async function (inputURL: string): Promise<Post> {
   return new Promise((resolve, reject) => {
     const url = new URL(inputURL);
     let found = false;
-    Instances.find( ([instance, reg]) => {
-      return reg.some(x => {
-        const match = url.pathname.match(x)
+    Instances.find(([instance, reg]) => {
+      return reg.some((x) => {
+        const match = url.pathname.match(x);
         if (!match) return;
         if (found) return;
 
         const postId = match[1];
-        const initInstance = new (instance)(url, postId);
-        initInstance.execute().then(res => resolve(res)).catch(err => reject(err))
-        found = true
+        const initInstance = new instance(url, postId);
+        initInstance
+          .execute()
+          .then((res) => resolve(res))
+          .catch((err) => reject(err));
+        found = true;
       });
     });
-    if (!found) reject(new Error('Invalid URL'))
+    if (!found) reject(new Error("Invalid URL"));
   });
 }
